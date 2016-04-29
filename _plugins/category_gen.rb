eaaaa6b7f59880a583ca6df7544fe13e0763c7e4
song <mssongan840136@outlook.com>
2016-04-29 09:53:54 +0900

    Modify the style of categoty

--- _plugins/category_gen.rb
+++ _plugins/category_gen.rb
@@ -11,7 +11,6 @@ module Jekyll
       self.read_yaml(File.join(base, '_layouts'), 'category_index.html')
       self.data['category'] = category
 
-      category_title_prefix = site.config['category_title_prefix'] || 'Category: '
     end
   end
 
