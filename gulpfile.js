9c26c119ff640bf8c3dd6984842cf41e0a1f3812
song <mssongan840136@outlook.com>
2020-10-29 00:46:59 +0900

    finished search features

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var minifyHTML = require('gulp-htmlmin');
 var changedInPlace = require('gulp-changed-in-place');
+var jsonminify = require('gulp-jsonminify');
 
 const DEST = './_site';
 const HTML_PATH = './_site/**/*.html';
+const JSON_PATH = './_site/*.json';
 
 gulp.task('minifyHTML', function() {
   return gulp.src(HTML_PATH)
@@ -11,3 +13,10 @@ gulp.task('minifyHTML', function() {
     .pipe(minifyHTML({collapseWhitespace: true}))
     .pipe(gulp.dest(DEST));
 });
+
+gulp.task('minifyJSON', function() {
+  return gulp.src(JSON_PATH)
+    .pipe(changedInPlace({firstPass: true}))
+    .pipe(jsonminify())
+    .pipe(gulp.dest(DEST));
+})
